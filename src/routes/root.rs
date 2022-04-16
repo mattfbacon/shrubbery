3615c335286080a3b9d4aa56c28649d63bf91cbd
Matt Fellenz <matt@felle.nz>
2022-04-16 16:50:08 -0700

    Flesh out homepage

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,13 +1,15 @@
+use crate::database::models::User;
 use crate::helpers::auth::Auth;
 use actix_web::Responder;
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "welcome.html")]
-struct WelcomeTemplate {}
+#[template(path = "home.html")]
+struct WelcomeTemplate {
+	user: User,
+}
 
 #[actix_web::get("/")]
-pub async fn handler(user: Auth) -> impl Responder {
-	todo!();
-	WelcomeTemplate {}
+pub async fn handler(Auth(user): Auth) -> impl Responder {
+	WelcomeTemplate { user }
 }
