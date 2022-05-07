5b859338f001ef338429f5b08e1b53b7d31231f4
Matt Fellenz <matt@felle.nz>
2022-05-07 15:33:58 -0700

    Add pagination helper

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,6 +1,7 @@
 use actix_web::cookie::Cookie;
 
 pub mod auth;
+pub mod pagination;
 
 pub fn internal_server_error<T>(err: T) -> actix_web::error::InternalError<T> {
 	actix_web::error::InternalError::new(err, actix_web::http::StatusCode::INTERNAL_SERVER_ERROR)
---
+++ src/helpers/pagination.rs
@@ -0,0 +1,78 @@
+pub type PageNum = i64;
+
+#[derive(serde::Deserialize, Clone, Copy)]
+pub struct Query {
+	#[serde(default = "default_page")]
+	pub page: PageNum,
+	#[serde(default = "default_page_size")]
+	pub page_size: PageNum,
+}
+
+const fn default_page() -> PageNum {
+	0
+}
+
+const fn default_page_size() -> PageNum {
+	20
+}
+
+impl Query {
+	pub fn offset(&self) -> PageNum {
+		self.page * self.page_size
+	}
+
+	pub fn limit(&self) -> PageNum {
+		self.page_size
+	}
+}
+
+#[derive(askama::Template, Clone, Copy)]
+#[template(path = "partials/pagination.html")]
+pub struct Template {
+	pub inner: Query,
+	pub num_pages: PageNum,
+}
+
+impl Template {
+	pub fn from_query(query: Query, num_pages: PageNum) -> Self {
+		Self {
+			inner: query,
+			num_pages,
+		}
+	}
+
+	pub fn href(&self) -> String {
+		format!(
+			"?page={}&page_size={}",
+			self.inner.page, self.inner.page_size
+		)
+	}
+
+	fn replace_page(&self, new: PageNum) -> Self {
+		Self {
+			num_pages: self.num_pages,
+			inner: Query {
+				page: new,
+				page_size: self.inner.page_size,
+			},
+		}
+	}
+
+	pub fn next_page(mut self) -> Option<Self> {
+		self.inner.page = self.inner.page.checked_add(1)?;
+		if self.inner.page < self.num_pages {
+			Some(self)
+		} else {
+			None
+		}
+	}
+
+	pub fn prev_page(mut self) -> Option<Self> {
+		self.inner.page = self.inner.page.checked_sub(1)?;
+		if self.inner.page >= 0 {
+			Some(self)
+		} else {
+			None
+		}
+	}
+}
