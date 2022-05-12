9e15f506a3f16d44a11d10b1b59341ce116fae22
Matt Fellenz <matt@felle.nz>
2022-05-11 20:05:30 -0700

    Use proper error type

--- src/routes/admin/users/delete.rs
+++ src/routes/admin/users/delete.rs
@@ -1,7 +1,5 @@
 use crate::database::{models, Database};
 use crate::helpers::auth;
-use actix_web::error::InternalError;
-use actix_web::http::StatusCode as HttpStatus;
 use actix_web::web::{self, ServiceConfig};
 use actix_web::{post, HttpRequest, HttpResponse, Responder};
 use ormx::Delete as _;
@@ -12,11 +10,9 @@ pub async fn post_handler(
 	req: HttpRequest,
 	path: web::Path<(models::UserId,)>,
 	database: web::Data<Database>,
-) -> Result<impl Responder, actix_web::Error> {
+) -> Result<impl Responder, super::Error> {
 	let user_id = path.into_inner().0;
-	models::User::delete_row(&**database, user_id)
-		.await
-		.map_err(|err| InternalError::new(err, HttpStatus::INTERNAL_SERVER_ERROR))?;
+	models::User::delete_row(&**database, user_id).await?;
 	Ok(
 		HttpResponse::SeeOther()
 			.insert_header(("Location", format!("/admin/users?{}", req.query_string())))
