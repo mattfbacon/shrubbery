3be92cd172e5200101915258d0a72dc85c0a2921
Matt Fellenz <matt@felle.nz>
2022-05-20 09:52:09 -0700

    Implement searching (untested)

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -7,11 +7,11 @@ mod root;
 mod tags;
 
 pub fn configure(app: &mut actix_web::web::ServiceConfig) {
-	root::configure(app);
 	admin::configure(app);
 	login::configure(app);
 	logout::configure(app);
 	register::configure(app);
+	root::configure(app);
 	tags::configure(app);
 	let files = actix_files::Files::new("/", "static")
 		.default_handler(actix_web::dev::fn_service(error::default_handler));
--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,18 +1,63 @@
-use crate::database::models::User;
-use crate::helpers::auth::Auth;
+use crate::database::{models, Database};
+use crate::helpers::auth;
+use crate::viewspec::ViewSpec;
+use actix_web::http::StatusCode as HttpStatus;
 use actix_web::{web, Responder};
-use askama::Template;
 
-#[derive(Template)]
+#[derive(askama::Template)]
 #[template(path = "index.html")]
-struct WelcomeTemplate {
-	self_user: User,
+struct Template {
+	self_user: models::User,
+	search_results: Option<Vec<(models::FileId, String)>>,
+	page_size: i64,
 }
 
-pub async fn handler(Auth(self_user): Auth) -> impl Responder {
-	WelcomeTemplate { self_user }
+#[derive(serde::Deserialize)]
+pub struct Query {
+	viewspec: Option<ViewSpec>,
+	after: Option<models::FileId>,
+	#[serde(default = "crate::helpers::pagination::default_page_size")]
+	page_size: i64,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+	#[error("{0}")]
+	Sqlx(#[from] sqlx::Error),
+}
+
+impl actix_web::ResponseError for Error {
+	fn status_code(&self) -> HttpStatus {
+		match self {
+			Self::Sqlx(..) => HttpStatus::INTERNAL_SERVER_ERROR,
+		}
+	}
+
+	fn error_response(&self) -> actix_web::HttpResponse {
+		crate::routes::error::error_response(self)
+	}
+}
+
+pub async fn get_handler(
+	auth::Auth(self_user): auth::Auth,
+	web::Query(Query {
+		viewspec,
+		after,
+		page_size,
+	}): web::Query<Query>,
+	database: web::Data<Database>,
+) -> Result<impl Responder, Error> {
+	let search_results = match viewspec {
+		Some(viewspec) => Some(viewspec.evaluate(&**database, after, page_size).await?),
+		None => None,
+	};
+	Ok(Template {
+		self_user,
+		search_results,
+		page_size,
+	})
 }
 
 pub fn configure(app: &mut actix_web::web::ServiceConfig) {
-	app.service(web::resource("/").route(web::get().to(handler)));
+	app.service(web::resource("/").route(web::get().to(get_handler)));
 }
