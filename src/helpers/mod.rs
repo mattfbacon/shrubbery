01afd5303d2051e467e10164803889a1ac8e5bf9
Matt Fellenz <matt@felle.nz>
2022-05-22 21:02:51 -0700

    Add io_error_to_status helper

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,4 +1,5 @@
 use actix_web::cookie::Cookie;
+use actix_web::http::StatusCode as HttpStatus;
 
 pub mod auth;
 pub mod or_null;
@@ -19,5 +20,14 @@ pub fn remove_cookie(name: &str) -> Cookie<'_> {
 pub fn set_none_if_empty(opt: &mut Option<String>) {
 	if opt.as_deref() == Some("") {
 		*opt = None;
+	}
+}
+
+pub fn io_error_to_status(err: &std::io::Error) -> HttpStatus {
+	use std::io::ErrorKind;
+	match err.kind() {
+		ErrorKind::NotFound => HttpStatus::NOT_FOUND,
+		ErrorKind::PermissionDenied => HttpStatus::FORBIDDEN,
+		_ => HttpStatus::INTERNAL_SERVER_ERROR,
 	}
 }
